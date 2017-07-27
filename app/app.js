15a6986dea7b92b2d6282a93806934c075138af5
jackharding <jackk.harding.93@gmail.com>
2017-07-27 21:57:39 +0100

    Added images. Finished basic quiz functionality

--- app/app.js
+++ app/app.js
@@ -43,6 +43,7 @@ readImages('./lists/files.txt');
 var app = new Vue({
 	el: '#app',
 	data: {
+		playing: true,
 		names: names,
 		images: images,
 		used: [],
@@ -52,25 +53,32 @@ var app = new Vue({
 			answers: [],
 		},
 		score: 0,
-		count: 1
+		count: 1,
+		limit: 10
 	},
 	methods: {
 		getRandomInt: function(array) {
 			var max = array.length - 1;
 			return Math.round(Math.random() * (max - 0));
 		},
-		getQuestion: function() {
+		getQuestion: function(unique) {
 			while(true) {
-				var question = this.names[this.getRandomInt(this.names)];
+				var question = this.names[this.getRandomInt(this.names)].trim();
 				// if this question has been used before, look again
-				if(!this.used.includes(question)) {
+				if(unique) {
+					console.log('test');
+					if(!this.used.includes(question)) {
+						return question;
+						break;
+					}					
+				} else {
 					return question;
 					break;
 				}
 			}
 		},
 		findMatch: function(needle, haystack) {
-			var matches = images.filter(function(word) {
+			var matches = haystack.filter(function(word) {
 				return word.includes(needle);
 			});
 			return matches[Math.floor(Math.random() * matches.length)];
@@ -78,9 +86,9 @@ var app = new Vue({
 		getAnswers: function() {
 			var answers = [];
 			for(var i = 0; i < 3; i++) {
-				var random = this.getQuestion();
+				var random = this.getQuestion(false);
 				// if the question is included in the answers or if it's a duplicate keep going
-				if(!answers.includes(random) || this.next.question != random) {
+				if(!answers.includes(random) && this.next.question != random) {
 					answers.push(random);
 				} else {
 					i--;
@@ -88,19 +96,32 @@ var app = new Vue({
 			}
 			return answers;
 		},
+		insertImage: function() {
+			var img = this.getQuestion(true);
+			this.next.question = img;
+			this.next.image = this.findMatch(this.next.question, images);
+
+			var canvas = document.getElementById('questionCanvas');
+			ctx = canvas.getContext('2d');
+			var canvasImg = new Image();
+			canvasImg.src = 'img/birds/' + this.next.image;
+			canvasImg.onload = function() {
+				ctx.drawImage(canvasImg, 0, 0, canvasImg.width, canvasImg.height, 0, 0, canvas.width, canvas.height);
+			}
+		},
 		getNext: function() {
 			if(this.next.question.length) {
 				this.used.push(this.next.question);
 			}
 
-			this.next.question = this.getQuestion();
-			this.next.image = this.findMatch(this.next.question, images);
+			this.insertImage();
 			this.next.answers = this.getAnswers();
 			this.next.answers.push(this.next.question);
 			this.next.answers = shuffleArray(this.next.answers);
 		},
 		submitAnswer: function(e) {
 			var choice = e.target.innerText;
+			console.log(choice, this.next.question.replace(/\d./g, '').replace(/_/g, ' '));
 			if(choice == this.next.question.replace(/\d./g, '').replace(/_/g, ' ')) {
 				this.score++;
 				console.log('correct', this.score);
@@ -108,7 +129,7 @@ var app = new Vue({
 				console.log('WRONG', this.score);
 			}
 
-			if(this.used.length == 14) {
+			if(this.used.length == this.limit - 1) {
 				this.endQuiz();
 			} else {
 				this.getNext();
@@ -117,6 +138,7 @@ var app = new Vue({
 		},
 		endQuiz: function() {
 			this.next = {};
+			this.playing = false;
 			alert('You got ' + this.score);
 		}
 	},
@@ -135,13 +157,13 @@ var app = new Vue({
 					document.querySelector('button[data-answer="0"]').click();
 					break;
 				case 50:
-					document.querySelector('button[data-answer="0"]').click();
+					document.querySelector('button[data-answer="1"]').click();
 					break;
 				case 51:
-					document.querySelector('button[data-answer="0"]').click();
+					document.querySelector('button[data-answer="2"]').click();
 					break;
 				case 52:
-					document.querySelector('button[data-answer="0"]').click();
+					document.querySelector('button[data-answer="3"]').click();
 					break;
 				default:
 					break;
