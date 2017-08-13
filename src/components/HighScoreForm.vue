b5f0b1df4b7dbbe9710684de8f058c7a22f30db3
Jack Harding <jackk.harding.93@gmail.com>
2017-08-13 21:59:45 +0100

    bit more on game over

---
+++ src/components/HighScoreForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="high-score">
+        <form class="" action="" method="post">
+            <input type="text" id="highScoreUsername">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
